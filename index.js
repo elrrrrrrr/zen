fa5a6e227a659d10bf812eafed083ee6ae574a04
elrrrrrrr <elrrrrrrr@gmail.com>
2014-12-24 20:25:16 +0800

    fetch data

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 var urllib = require('urllib');
+var list = require('./data.json').dataList || [];
+var fs = require('fs');
 
 var url = 'https://api.github.com/zen';
 
 var option = {
-  timeout: 500000
+  timeout: 10000
 }
 
-urllib.request(url, option, function (err, data, res) {
-  if (err) {
-    console.log(err);
-    //TODO
+
+function hasEnough() {
+  var result = false;
+  if (list.length > 10) {
+    result = true;
   }
-  console.log(data.toString());
-});
+  return result;
+}
+
+function doFetch(cb) {
+  urllib.request(url, option, function (err, data) {
+    if (err) {
+      //console.log(err);
+      return cb();
+      //TODO
+    }
+    var result = data.toString();
+    if (list.indexOf(result) == -1) {
+      list.push(result);
+    }
+    console.log(result);
+    cb && cb();
+  });
+}
+
+function saveData() {
+
+  if (hasEnough()) {
+    fs.writeFileSync('./data.json', JSON.stringify(list, 2, 2));  
+  } else {
+    doFetch(saveData);
+  }
+}
+
+doFetch(saveData)
